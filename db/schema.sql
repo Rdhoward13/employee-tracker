b8bf5c72b3fdac38933ee81cf544e0602e61ed5c
rachel howard <rdfields13@gmail.com>
2022-12-01 14:13:10 -0600

    fixed schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,30 @@ CREATE DATABASE enployeeTracker_db;
 
 USE employeeTracker_db;
 
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)
+
 CREATE TABLE roles (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  department INTEGER(30),
-  PRIMARY KEY (id)
+  department_id INTEGER(30),
+  salary INTEGER (30)
+  FOREIGN KEY (department_id)
+  REFERENCES departments(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-  id INT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
-  last_name INT,
-  role_id TEXT,
-  FOREIGN KEY (employ_id)
-  REFERENCES instructors(id)
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
   ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO roles (department_id, title, salary)
+VALUES (1, "Intern", 40000),
+       (2, "Engineer"),
+       ("Manager"),
+       ("Sales Manager"),
+       ("Business Development Rep"),
+       ("Game Design"),
+       ("Cloud Development");
