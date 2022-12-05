90adaf5831c5b87f77b3cc33af026a2f366f7384
rachel howard <rdfields13@gmail.com>
2022-12-04 21:12:03 -0600

    updated prompts for employees, departments

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,9 @@
 INSERT INTO roles (department_id, title, salary)
-VALUES (1, "Intern", 40000),
-       (2, "Engineer"),
-       ("Manager"),
-       ("Sales Manager"),
-       ("Business Development Rep"),
-       ("Game Design"),
-       ("Cloud Development");
+VALUES (1, "Salesperson", 80000),
+       (2, "Lead Engineer", 150000),
+       (3, "Software Engineer", 120000),
+       (4, "Account Manager", 160000),
+       (5, "Accountant", 125000),
+       (6, "Legal Team Lead", 250000),
+       (7, "Lawyer", 190000);
+       (8. "Sales Lead", 10000)
